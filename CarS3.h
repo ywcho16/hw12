b1499a9c27a18f48e8e9ad13a6283a36803a954f
ywcho16 <163232974+ywcho16@users.noreply.github.com>
2024-05-24 14:27:32 +0900

    Add files via upload

---
+++ CarS3.h
@@ -0,0 +1,22 @@
+#pragma once
+class Car {
+public:
+	Car() :speed(0.1), wheels(0), price(0.2) {};
+	Car(float, int, float);
+	void setPrice(float);
+	float getPrice();
+	float speed;
+	void speedChange(float);
+	int wheels;
+	void setWheels(int);
+	int getWheels();
+	void klaxon(int);
+	int efficiency();
+protected:
+	float price;
+	void speedUp(float);
+	void speedDown(float);
+	float design;
+private:
+	float madeby;
+};
